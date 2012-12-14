855213f0bfb0fe003c0f98338ca673105de4d950
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2012-12-14 18:32:59 +1100

    Issue #5295 for Redmine 2.0.3. Fixed specific symbol $

--- assets/javascripts/docflows.js
+++ assets/javascripts/docflows.js
@@ -1,7 +1,14 @@
+// Case-insensetive search for jQuery >= 1.8
 jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
-    return function( elem ) {
-        return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-    };
+     return function( elem ) {
+         return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+     };
+ });
+
+// Case-insensetive search for jQuery < 1.8
+//jQuery.expr[':'].Contains = function(elem, index, match) {
+//  return jQuery(elem).text().toUpperCase().indexOf(match[3].toUpperCase()) >= 0;
+//};
 
 function modify_comment_form(obj){
     jQuery('#comment-new-title').html(jQuery(obj).find("span").html());
